0b9c9de22e1e477ab0141c92348530010d47b7e6
bradcgarner <brad@bradgarner.com>
2017-10-17 06:51:38 -0400

    integrated mongo to server

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 'use strict';
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const app = express();
+
+const {PORT, CLIENT_ORIGIN, DATABASE_URL} = require('./config');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 const { router: userRouter } = require('./users');
 const { router: quizRouter } = require('./quizzes');
 
-const {PORT, CLIENT_ORIGIN} = require('./config');
-const {dbConnect} = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
-
-const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -27,16 +28,34 @@ app.use(
 app.use('/api/users', userRouter);
 app.use('/api/quizzes', quizRouter);
 
+function dbConnect(url = DATABASE_URL) {
+  return mongoose.connect(DATABASE_URL, {useMongoClient: true}).catch(err => {
+    console.error('Mongoose failed to connect');
+    console.error(err);
+  });
+}
 
-function runServer(port = PORT) { 
-  const server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
+let server;
+
+function runServer(url = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(url, { useMongoClient: true }, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+        .listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+        .on('error', err => {
+          console.error('Express failed to start');
+          console.error(err);
+          mongoose.disconnect();
+          reject(err);
+        });
     });
+  });
 }
 
 if (require.main === module) {
